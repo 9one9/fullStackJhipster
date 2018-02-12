54d06a532935fc89e4d8dcd20ede01bb06cd5396
9one9 <brayson@albany.edu>
2018-02-12 12:12:18 -0500

    Initial application generated by JHipster-4.14.0

---
+++ src/main/webapp/app/shared/shared.module.ts
@@ -0,0 +1,54 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { DatePipe } from '@angular/common';
+
+import {
+    XSqlJhipsterSharedLibsModule,
+    XSqlJhipsterSharedCommonModule,
+    CSRFService,
+    AuthServerProvider,
+    AccountService,
+    UserService,
+    StateStorageService,
+    LoginService,
+    LoginModalService,
+    JhiLoginModalComponent,
+    Principal,
+    HasAnyAuthorityDirective,
+    JhiSocialComponent,
+    SocialService,
+} from './';
+
+@NgModule({
+    imports: [
+        XSqlJhipsterSharedLibsModule,
+        XSqlJhipsterSharedCommonModule
+    ],
+    declarations: [
+        JhiSocialComponent,
+        JhiLoginModalComponent,
+        HasAnyAuthorityDirective
+    ],
+    providers: [
+        LoginService,
+        LoginModalService,
+        AccountService,
+        StateStorageService,
+        Principal,
+        CSRFService,
+        AuthServerProvider,
+        SocialService,
+        UserService,
+        DatePipe
+    ],
+    entryComponents: [JhiLoginModalComponent],
+    exports: [
+        XSqlJhipsterSharedCommonModule,
+        JhiSocialComponent,
+        JhiLoginModalComponent,
+        HasAnyAuthorityDirective,
+        DatePipe
+    ],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
+
+})
+export class XSqlJhipsterSharedModule {}
